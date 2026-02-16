d3bc69ed1a7a4db59f0e7d56987280def605d350
Parmeet <parmeetbhamrah@gmail.com>
2026-02-17 00:47:44 +0530

    fix prisma adapter related issues due to version and create register route

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,12 @@
 // this file configures the connection to the database
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
+const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaPg({ connectionString });
+
 const prisma = new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === "development"
     ? ["query", "error", "warn"] 
     : ["error"]
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { connectDB, disconnectDB } from './config/db.js'
 
 //import routes
 import movieRoutes from './routes/movieRoutes.js'
+import authRoutes from "./routes/authRoutes.js"
 
 config();
 connectDB();
@@ -12,6 +13,7 @@ const app = express();
 
 // API Routes
 app.use("/movies", movieRoutes);
+app.use("/auth", authRoutes);
 
 const PORT = 5001;
 app.listen(PORT, () => {
